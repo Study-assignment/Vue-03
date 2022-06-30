317037edf06366fe053c4ab306353e36415d71e3
renbuairen <1664844124@qq.com>
2022-06-30 20:01:51 +0800

    动态class

---
+++ src/04_动态class.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p :class="{ active: isActive }" class="ppp">我是乒乒乓乓</p>
+
+    <button @click="isActive = !isActive">点我</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: true,
+    }
+  },
+
+  mounted() {},
+
+  methods: {},
+}
+</script>
+<style>
+.active {
+  color: red;
+}
+.ppp {
+  border: 5px solid rgb(26, 223, 191);
+}
+</style>

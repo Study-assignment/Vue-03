5a294989ecf2590639d4744c213ac51fe4df8733
renbuairen <1664844124@qq.com>
2022-06-30 20:04:54 +0800

    动态style

---
+++ src/05_动态style.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p v-bind:style="{ color: 'red', fontSize: '100px' }">asdasd</p>
+    <p v-bind:style="{ color: colorColor, fontSize: `${fontSize}px` }">
+      asdasd
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colorColor: 'green',
+      fontSize: 100,
+    }
+  },
+}
+</script>
+
+<style></style>

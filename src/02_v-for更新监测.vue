032b102a990a20b8c5131730fbb52e1606b939f1
renbuairen <1664844124@qq.com>
2022-06-30 19:55:03 +0800

    v-for更新监测

---
+++ src/02_v-for更新监测.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in arr" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="reverse">数组翻转</button>
+    <button @click="slice">截取前3个</button>
+    <button @click="change">更新第一个元素值</button>
+    <button @click="addVal">在0上添加一个值</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      arr: [1, 2, 3, 4, 5],
+    }
+  },
+  methods: {
+    reverse() {
+      this.arr.reverse()
+    },
+    slice() {
+      this.arr = this.arr.slice(0, 3)
+    },
+    change() {
+      this.$set(this.arr, 0, 1000)
+    },
+    addVal() {
+      this.arr.splice(0, 0, 50)
+    },
+  },
+}
+</script>
+
+<style scoped></style>

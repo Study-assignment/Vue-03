830384bdadd1639dc4547b665b371dd35c25d37e
bianling <1343881168@qq.com>
2022-06-30 21:10:17 +0800

    完成了学生信息管理案例

--- src/App.vue
+++ src/App.vue
@@ -1,163 +1,129 @@
 <template>
-  <div class="wrap">
-    <div class="nav_left" id="navLeft">
-      <div class="nav_content">
-        <span :class="{active:item.first_id==page }" v-for="item in arr" :key="item.first_id" @click="page=item.first_id">{{item.first_name}}</span>
-      </div>
+  <div id="app">
+    <div>
+      <span>姓名:</span>
+      <input type="text" v-model.trim="name" />
     </div>
-    <div class="down">
-      <i class="iconfont icon-xiajiantoubeifen gray"></i>
+    <div>
+      <span>年龄:</span>
+      <input type="number" v-model.trim="age" />
+    </div>
+    <div>
+      <span>性别:</span>
+      <select v-model="sex" >
+        <option value="男">男</option>
+        <option value="女">女</option>
+      </select>
+    </div>
+    <div>
+      <button @click="add">添加/修改</button>
+    </div>
+    <div>
+      <table
+        border="1"
+        cellpadding="10"
+        cellspacing="0"
+      >
+        <tr>
+          <th>序号</th>
+          <th>姓名</th>
+          <th>年龄</th>
+          <th>性别</th>
+          <th>操作</th>
+        </tr>
+        <tr v-for="item in peoples" :key="item.id">
+          <td>{{item.id}}</td>
+          <td>{{item.name}}</td>
+          <td>{{item.age}}</td>
+          <td>{{item.sex}}</td>
+          <td>
+            <button @click="remove(item.id)">删除</button>
+            <button @click="edit(item)">编辑</button>
+          </td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
-
 <script>
 export default {
-  data() {
-    return {
-      arr: [
-        {
-          first_id: "0",
-          first_name: "热门"
-        },
-        {
-          first_id: "621",
-          first_name: "\u5496\u5561",
-        },
-        {
-          first_id: "627",
-          first_name: "\u996e\u98df",
-        },
-        {
-          first_id: "279",
-          first_name: "\u7537\u88c5",
-        },
-        {
-          first_id: "294",
-          first_name: "\u5973\u88c5",
-        },
-        {
-          first_id: "122",
-          first_name: "\u773c\u955c",
-        },
-        {
-          first_id: "339",
-          first_name: "\u5185\u8863\u914d\u9970",
-        },
-        {
-          first_id: "391",
-          first_name: "\u6bcd\u5a74",
-        },
-        {
-          first_id: "35",
-          first_name: "\u978b\u9774",
-        },
-        {
-          first_id: "39",
-          first_name: "\u8fd0\u52a8",
-        },
-        {
-          first_id: "153",
-          first_name: "\u7bb1\u5305",
-        },
-        {
-          first_id: "119",
-          first_name: "\u7f8e\u5986\u4e2a\u62a4",
-        },
-        {
-          first_id: "355",
-          first_name: "\u5bb6\u7eba",
-        },
-        {
-          first_id: "51",
-          first_name: "\u9910\u53a8",
-        },
-        {
-          first_id: "334",
-          first_name: "\u7535\u5668",
-        },
-        {
-          first_id: "369",
-          first_name: "\u5bb6\u88c5",
-        },
-        {
-          first_id: "10",
-          first_name: "\u5bb6\u5177",
-        },
-        {
-          first_id: "223",
-          first_name: "\u6570\u7801",
-        },
-        {
-          first_id: "429",
-          first_name: "\u6c7d\u914d",
-        },
-        {
-          first_id: "546",
-          first_name: "\u5065\u5eb7\u4fdd\u5065",
-        },
-        {
-          first_id: "433",
-          first_name: "\u5b9a\u5236",
-        },
-      ],
-      page:0
-    };
-  },
+    data(){
+      return{
+        peoples:[
+          {
+            id:1,
+            name:'小西施',
+            age:18,
+            sex:'男'
+          },
+               {
+            id:2,
+            name:'大西施',
+            age:17,
+            sex:'女'
+          },
+              {
+            id:3,
+            name:'张三',
+            age:20,
+            sex:'女'
+          }
+        ],
+        name:'',
+        age:'',
+        sex:'男',
+        flag: true,
+        editId:0
+      }
+    },
+    methods:{
+      //编辑确认前
+      edit(item){
+        this.flag=false
+        this.name = item.name
+        this.age = item.age
+        this.sex=item.sex
+        this.editId = item.id
+      },
+      //添加和修改
+      add(){
+        if(!this.name ||!this.age) return alert('姓名或者年龄不能为空')
+         if(this.flag){
+          //正常添加
+         this.peoples.push({
+          id:this.peoples[0]?this.peoples[this.peoples.length-1].id+1:1,
+          name:this.name,
+          age:this.age,
+          sex:this.sex
+        })
+        this.name=''
+        this.age=''
+       }else{
+        //编辑
+        const index = this.peoples.findIndex(item=>this.editId==item.id)
+        this.peoples.splice(index,1,{
+          id:this.editId,
+          name:this.name,
+          age:this.age,
+          sex:this.sex
+        })
+        this.name=''
+        this.age=''
+        this.flag=true
+       }
+      },
+      //删除
+      remove(id){
+        const index = this.peoples.findIndex(item=>item.id==id)
+        this.peoples.splice(index,1)
+      }
+    }
 
-};
+}
 </script>
 
 <style>
-.wrap {
-  width: 100%;
-  display: flex;
-  margin: 0.2rem 0 0 0;
-  position: relative;
-}
-
-/*左侧的导航样式*/
-.nav_left {
-  width: 21.1875rem;
-  overflow: scroll;
-}
-
-.nav_left::-webkit-scrollbar {
-  display: none;
-}
-
-.nav_content {
-  white-space: nowrap;
-  padding: 0 0.7rem;
-}
-
-.nav_content span {
-  display: inline-block;
-  padding: 0.4rem 0.6rem;
-  font-size: 0.875rem;
-}
-
-.nav_content .active {
-  border-bottom: 2px solid #7f4395;
-  color: #7f4395;
-}
-
-.nav_left,
-.down {
-  float: left;
-}
-
-/*右侧导航部分*/
-.down {
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.gray {
-  color: gray;
-  display: inline-block;
-  vertical-align: middle;
-}
+    #app{
+      margin-left: 600px;
+    }
 </style>

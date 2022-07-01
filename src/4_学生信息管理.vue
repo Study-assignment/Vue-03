2f7af7975d7213c9e0055ac467c2e1d0f6942282
xx0tt <307457823@qq.com>
2022-07-01 10:11:22 +0800

    优化学生信息

--- src/4_学生信息管理.vue
+++ src/4_学生信息管理.vue
@@ -2,21 +2,29 @@
   <div id="app">
     <div>
       <span>姓名:</span>
-      <input type="text" v-model.trim="name" />
+      <input
+        type="text"
+        v-model.trim="userInfo.name"
+        placeholder="请输入姓名"
+      />
     </div>
     <div>
       <span>年龄:</span>
-      <input type="number" v-model.number="age" />
+      <input
+        type="number"
+        v-model.number="userInfo.age"
+        placeholder="请输入年龄"
+      />
     </div>
     <div>
       <span>性别:</span>
-      <select v-model="sex">
-        <option value="男">男</option>
-        <option value="女">女</option>
+      <select v-model="userInfo.sex">
+        <option :value="1">男</option>
+        <option :value="0">女</option>
       </select>
     </div>
     <div>
-      <button @click="addFn">添加/修改</button>
+      <button @click="addFn">{{ flag ? '修改' : '添加' }}</button>
     </div>
     <div>
       <table border="1" cellpadding="10" cellspacing="0">
@@ -31,10 +39,10 @@
           <td>{{ item.id }}</td>
           <td>{{ item.name }}</td>
           <td>{{ item.age }}</td>
-          <td>{{ item.sex }}</td>
+          <td>{{ { 0: '女', 1: '男' }[item.sex] }}</td>
           <td>
             <button @click="del(item.id)">删除</button>
-            <button @click="edit(item.id)">编辑</button>
+            <button @click="edit(item)">编辑</button>
           </td>
         </tr>
       </table>
@@ -47,13 +55,15 @@ export default {
   data() {
     return {
       flag: 0,
-      name: '',
-      age: '',
-      sex: '男',
+      userInfo: {
+        name: '',
+        age: '',
+        sex: 1,
+      },
       list: [
-        { id: 1, name: '张三', age: 20, sex: '男' },
-        { id: 2, name: '李四', age: 22, sex: '男' },
-        { id: 3, name: '韩梅梅', age: 19, sex: '女' },
+        { id: 1, name: '张三', age: 20, sex: 1 },
+        { id: 2, name: '李四', age: 22, sex: 1 },
+        { id: 3, name: '韩梅梅', age: 19, sex: 0 },
       ],
     }
   },
@@ -64,38 +74,31 @@ export default {
         if (this.name == '' || this.age == '') return alert('请输入完整')
         this.list.push({
           id: this.list.length ? this.list[this.list.length - 1].id + 1 : 1,
-          name: this.name,
-          age: this.age,
-          sex: this.sex,
+          ...this.userInfo,
         })
-        this.name = ''
-        this.age = ''
-        this.sex = '男'
-        return
+      } else {
+        // 修改
+        const index = this.list.findIndex((item) => item.id == this.flag)
+        this.$set(this.list, index, {
+          id: this.flag,
+          ...this.userInfo,
+        })
+        alert('修改完成')
+        this.flag = 0
       }
-      // 修改
-      const index = this.list.findIndex((item) => item.id == this.flag)
-      this.$set(this.list, index, {
-        id: this.flag,
-        name: this.name,
-        age: this.age,
-        sex: this.sex,
-      })
-      this.flag = 0
-      this.name = ''
-      this.age = ''
-      this.sex = '男'
+      this.userInfo.name = ''
+      this.userInfo.age = ''
+      this.userInfo.sex = 1
     },
     del(id) {
       const index = this.list.findIndex((item) => item.id == id)
       this.list.splice(index, 1)
     },
-    edit(id) {
-      const item = this.list.filter((item) => item.id == id)
-      this.name = item[0].name
-      this.age = item[0].age
-      this.sex = item[0].sex
-      this.flag = id
+    edit(item) {
+      this.userInfo.name = item.name
+      this.userInfo.age = item.age
+      this.userInfo.sex = item.sex
+      this.flag = item.id
     },
   },
 }
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,105 @@
 <template>
-  <div>
-    <div
-      v-for="(item, index) in arr"
-      :key="index"
-      style="display: inline-block"
-    >
-      <input type="checkbox" :value="item" v-model="num" /> {{ item }}
-      &nbsp;
+  <div id="app">
+    <div>
+      <span>姓名:</span>
+      <input
+        type="text"
+        v-model.trim="userInfo.name"
+        placeholder="请输入姓名"
+      />
     </div>
-    <p>你选中的元素, 累加的值和为: {{ theSum }}</p>
+    <div>
+      <span>年龄:</span>
+      <input
+        type="number"
+        v-model.number="userInfo.age"
+        placeholder="请输入年龄"
+      />
+    </div>
+    <div>
+      <span>性别:</span>
+      <select v-model="userInfo.sex">
+        <option :value="1">男</option>
+        <option :value="0">女</option>
+      </select>
+    </div>
+    <div>
+      <button @click="addFn">{{ flag ? '修改' : '添加' }}</button>
+    </div>
+    <div>
+      <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+          <th>序号</th>
+          <th>姓名</th>
+          <th>年龄</th>
+          <th>性别</th>
+          <th>操作</th>
+        </tr>
+        <tr v-for="item in list" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.age }}</td>
+          <td>{{ { 0: '女', 1: '男' }[item.sex] }}</td>
+          <td>
+            <button @click="del(item.id)">删除</button>
+            <button @click="edit(item)">编辑</button>
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
+  name: 'App',
   data() {
     return {
-      num: [],
-      arr: [9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+      flag: 0,
+      userInfo: {
+        name: '',
+        age: '',
+        sex: 1,
+      },
+      list: [
+        { id: 1, name: '张三', age: 20, sex: 1 },
+        { id: 2, name: '李四', age: 22, sex: 1 },
+        { id: 3, name: '韩梅梅', age: 19, sex: 0 },
+      ],
     }
   },
-  computed: {
-    theSum() {
-      return this.num.reduce((pre, item) => pre + item, 0)
+  methods: {
+    addFn() {
+      if (!this.flag) {
+        // 添加
+        if (this.name == '' || this.age == '') return alert('请输入完整')
+        this.list.push({
+          id: this.list.length ? this.list[this.list.length - 1].id + 1 : 1,
+          ...this.userInfo,
+        })
+      } else {
+        // 修改
+        const index = this.list.findIndex((item) => item.id == this.flag)
+        this.$set(this.list, index, {
+          id: this.flag,
+          ...this.userInfo,
+        })
+        alert('修改完成')
+        this.flag = 0
+      }
+      this.userInfo.name = ''
+      this.userInfo.age = ''
+      this.userInfo.sex = 1
+    },
+    del(id) {
+      const index = this.list.findIndex((item) => item.id == id)
+      this.list.splice(index, 1)
+    },
+    edit(item) {
+      this.userInfo.name = item.name
+      this.userInfo.age = item.age
+      this.userInfo.sex = item.sex
+      this.flag = item.id
     },
   },
 }
 </script>
-
-<style></style>

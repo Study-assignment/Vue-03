50a9f0ffc5812be8164bcdd0106576aad89b5f21
renbuairen <1664844124@qq.com>
2022-07-01 08:15:35 +0800

    选你爱我求和

---
+++ src/附加练习_5.选你爱我求和.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div
+      v-for="(item, index) in arr"
+      style="display: inline-block"
+      :key="index"
+    >
+      <input
+        type="checkbox"
+        v-model="checkNumArr"
+        :value="item"
+        @change="theSum"
+      />
+      <span>{{ item }}</span>
+    </div>
+    <p>你选中的元素, 累加的值和为: {{ sum }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sum: 0,
+      arr: [9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+      checkNumArr: [], //  保存用户选中的数字
+    }
+  },
+  methods: {
+    theSum() {
+      this.sum = this.checkNumArr.reduce((sum, val) => {
+        return sum + val
+      }, 0)
+    },
+  },
+}
+</script>
+
+<style></style>

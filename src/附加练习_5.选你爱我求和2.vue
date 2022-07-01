fe2d8739e37d964c4fa710e7334c583f7851e544
renbuairen <1664844124@qq.com>
2022-07-01 17:51:36 +0800

    计算属性方法

---
+++ src/附加练习_5.选你爱我求和2.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-for="item in arr" :key="item" style="display: inline-block">
+      <input type="checkbox" v-model="checkNumArr" :value="item" /> {{ item }}
+    </div>
+    求和：{{ getToTal }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+      checkNumArr: [],
+    }
+  },
+
+  computed: {
+    getToTal() {
+      return this.checkNumArr.reduce((sum, next) => sum + next, 0)
+    },
+  },
+}
+</script>
+
+<style scoped></style>
